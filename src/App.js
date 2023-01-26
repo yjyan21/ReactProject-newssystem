b1815db16b9963203628a9b218672d67978ab7e1
yjyan21 <yjyan1987@gmail.com>
2023-01-25 16:25:40 -0800

    chapter 155 bilibili

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Child from './Child'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  useEffect( ()=> {
+    axios.get("/api/mmdb/movie/v3/list/hot.json?ct=%E5%8C%97%E4%BA%AC&ci=1&channelId=4").then(res=>{
+      console.log(res.data)
+    })
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>App
+      <ul>
+        <li>111111111</li>
+        <li>111111111</li>
+      </ul>
+      <Child />
     </div>
   );
 }
