1e1f8b4f12961059a47eb1246191aa87ebf60233
yjyan21 <yjyan1987@gmail.com>
2023-01-26 16:19:03 -0800

    update route etc.

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {HashRouter, Route, Switch, Redirect} from 'react-router-dom'
 import Login from '../views/login/Login'
 import NewsSandBox from '../views/sandbox/NewsSandBox'
 
 export default function IndexRouter() {
-  return (
-    <div>
-     	<Router>
-        <Routes>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/" element={<NewsSandBox/>}/>
-        </Routes>
-    </Router> 
-    </div>
-  )
+	return (
+			<HashRouter>
+				<Switch>
+					<Route path="/login" component={Login} />
+					{/* <Route path="/" component={NewsSandBox} /> */}
+					<Route path="/" render={()=>
+						localStorage.getItem("token")? 
+						<NewsSandBox></NewsSandBox>:
+						<Redirect to="/login" />
+					} />
+				</Switch>
+			</HashRouter>
+	)
 }
+
--- src/views/sandbox/NewsSandBox.jsx
+++ src/views/sandbox/NewsSandBox.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import SideMenu from '../../components/sandbox/SideMenu'
+import TopHeader from '../../components/sandbox/TopHeader'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import Home from './home/Home'
+import UserList from './user-manage/UserList'
+import RoleList from './right-manage/RoleList'
+import RightList from './right-manage/RightList'
+import NoPermission from './nopermission/NoPermission'
 
 export default function NewsSandBox() {
   return (
-    <div>NewsSandBox</div>
+    <div>
+      <SideMenu></SideMenu>  
+			<TopHeader></TopHeader> 
+			<Switch>
+				<Route path="/home" component={Home} />
+				<Route path="/user-manager/list" component={UserList} />
+				<Route path="/right-manager/role/list" component={RoleList} />
+				<Route path="/right-manager/right/list" component={RightList} />
+
+				<Redirect from="/" to="Home" exact />
+				<Route path="*" component={NoPermission} />
+			</Switch>
+    </div>
   )
 }
