3ab7f9c91c6283ef9c0c6288034834ea9fb9ef38
yjyan21 <yjyan1987@gmail.com>
2023-01-31 21:17:40 -0800

    update home page

--- src/components/sandbox/TopHeader.js
+++ src/components/sandbox/TopHeader.js
@@ -1,18 +1,40 @@
 import React, { useState } from 'react'
-import { Layout, Menu, theme } from 'antd';
+import { Layout, Avatar, Dropdown, Space } from 'antd';
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
+  UserOutlined,
+  DownOutlined,
+  SmileOutlined
 } from '@ant-design/icons';
 const { Header } = Layout; //import语句都写完了，才能写const语句；
 
 export default function TopHeader() {
   const[collapsed,setCollapsed] = useState(false)
+  const changeCollapsed = () => {
+    setCollapsed(!collapsed)
+  }
+  const items = [
+    {
+      key: '1',
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+          Admin
+        </a>
+      ),
+    },
+   
+    {
+      key: '4',
+      danger: true,
+      label: 'Sign out',
+    },
+  ];
   return (
     <div>
       <Header
           style={{
-            padding: 0,
+            padding: '0px 16px',
             // background: colorBgContainer,
           }}
         >
@@ -21,9 +43,30 @@ export default function TopHeader() {
             onClick: () => setCollapsed(!collapsed),
           })} */}
           {
-            collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />
+            collapsed ? <MenuUnfoldOutlined onClick={changeCollapsed}/> : <MenuFoldOutlined 
+            onClick={changeCollapsed}/>
           }
+          
+          <span style={{float:'right'}}>
+           {/*  */}
+           <Dropdown
+              menu={{
+                items,
+              }}
+            >
+              <a onClick={(e) => e.preventDefault()}>
+                <Space>
+                <Avatar size="small" icon={<UserOutlined />} />
+                  {/* <DownOutlined /> 小图标右边的向下的箭头图标*/}
+                </Space>
+              </a>
+          </Dropdown>
+          </span>
+          <span style={{float:'right',paddingRight:'5px'}}>
+            Welcome back! admin  
+          </span>
         </Header>
+        
     </div>
   )
 }
