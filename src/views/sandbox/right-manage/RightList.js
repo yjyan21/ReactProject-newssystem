da37cf8a1e1e4f48b159ff60847f87a61a3562b8
yjyan21 <yjyan1987@gmail.com>
2023-02-10 17:39:09 -0800

    update RightList.js

--- src/views/sandbox/right-manage/RightList.js
+++ src/views/sandbox/right-manage/RightList.js
@@ -1,7 +1,10 @@
 import React, {useState, useEffect} from 'react'
-import {Table, Tag, Button} from 'antd'
-import {DeleteOutlined, EditOutlined} from '@ant-design/icons'
+import {Table, Tag, Button, Modal} from 'antd'
+import {DeleteOutlined, EditOutlined, ExclamationCircleFilled} from '@ant-design/icons'
+
 import axios from 'axios'
+
+const { confirm } = Modal;
 
 export default function RightList() {
   const [dataSource, setDataSource] = useState([])
@@ -9,7 +12,11 @@ export default function RightList() {
     axios.get("http://localhost:5000/rights?_embed=children").then
     (res=> {
       let datalist = res.data
-      datalist[0].children = ""
+      datalist.forEach(item=>{
+        if(item.children.length===0){
+          item.children = ""
+        }
+      })
       setDataSource(res.data)
     })
 })
@@ -34,15 +41,50 @@ export default function RightList() {
     },
     {
       title: '操作',
-      render:()=>{
+      render:(item)=>{
         return (
           <div>
-            <Button danger shape="circle" icon={<DeleteOutlined />} />
+            <Button danger shape="circle" icon={<DeleteOutlined />} 
+            onClick={()=>{ //这里的形参括号里面不能写item,否则传下去的参数就不一样了
+              confirmMethod(item);
+            }}/>
             <Button type="primary" shape="circle" icon={<EditOutlined />} />
           </div>)
         }
     },
   ];
+
+  const confirmMethod = (item) => {
+    confirm({
+      title: 'Do you Want to delete these items?',
+      icon: <ExclamationCircleFilled />,
+      // content: 'Some descriptions',
+      onOk() {
+        // console.log('OK');
+        deleteMethod(item);
+      },
+      onCancel() {
+        // console.log('Cancel');
+      },
+    });
+  };
+  const deleteMethod = (item) => {
+    //console.log(item)
+    if (item.grade === 1) {
+      setDataSource(dataSource.filter(data => data.id !== item.id))
+      axios.delete(`http://localhost:5000/rights/${item.id}`)
+    }else{
+      //console.log(item.rightId)
+      let list = dataSource.filter(data=>data.id===item.rightId)
+      list[0].children = list[0].children.filter(data=>data.id!==item.id)
+      //console.log(list,dataSource)
+      setDataSource([...dataSource])
+      axios.delete(`http://localhost:5000/children/${item.id}`)
+    }
+
+
+  }
+
   return (
     <div>
       <Table dataSource={dataSource} columns={columns} 
