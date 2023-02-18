e5c82413ff7088973f0df62bd91dfa92321fe00e
yjyan21 <yjyan1987@gmail.com>
2023-02-18 10:52:54 -0800

    update NewsAdd.js

--- src/views/sandbox/news-manage/NewsAdd.js
+++ src/views/sandbox/news-manage/NewsAdd.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {PageHeader} from '@ant-design/pro-layout'
+import { Steps } from 'antd';
 
 export default function NewsAdd() {
+  //const description = 'This is a description.';
+  const [current, SetCurrent] = useState(0)
   return (
-    <div>NewsAdd</div>
+    <div>
+      <PageHeader
+        className="site-page-header" 
+        title="Write News"
+        subTitle="This is a subtitle" 
+        />
+      <Steps
+        current={current}
+        items={[
+          {
+            title: 'Basic Information',
+            description: 'Title, Category',
+          },
+          {
+            title: 'News Content',
+            description: 'News topic content',
+            // subTitle: 'Left 00:00:08',
+          },
+          {
+            title: 'Waiting audit',
+            description: 'Draft or waiting audit',
+          },
+        ]}
+      />
+    </div>
   )
 }
