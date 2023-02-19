490ebad87a4ce3a754f6dc8999e07d4fa06b2420
yjyan21 <yjyan1987@gmail.com>
2023-02-18 20:54:41 -0800

    update NewsAdd.js

--- src/views/sandbox/news-manage/NewsAdd.js
+++ src/views/sandbox/news-manage/NewsAdd.js
@@ -1,10 +1,18 @@
 import React,{useState} from 'react'
 import {PageHeader} from '@ant-design/pro-layout'
-import { Steps } from 'antd';
+import { Steps, Button } from 'antd';
+import style from './News.css'
 
 export default function NewsAdd() {
   //const description = 'This is a description.';
-  const [current, SetCurrent] = useState(0)
+  
+  const [current, setCurrent] = useState(2)
+  const handlePrevious = () => {
+    setCurrent(current-1)
+  }
+  const handleNext = () => {
+    setCurrent(current+1)
+  }
   return (
     <div>
       <PageHeader
@@ -30,6 +38,34 @@ export default function NewsAdd() {
           },
         ]}
       />
+      
+      <div className={current===0?'':style.active}>1111111
+        <input type="text"/>
+      </div>
+      <div className={current===1?'':style.active}>22222
+        <input type="text"/>
+      </div>
+      <div className={current===2?'':style.active}>3333
+        <input type="text"/>
+      </div>
+
+      <div style={{marginTop:'50px'}}>
+        {
+          current>0 && <span>
+            <Button type="primary" onClick={()=>handlePrevious()}>上一步</Button>
+            </span>
+        }
+        {
+          current === 2 && <span>
+            <Button type="primary">Save Draft</Button>
+            <Button danger>Submit to Audit</Button>
+            </span>
+        }
+        {
+          current<2 && <Button type="primary" onClick={()=>handleNext()}>下一步</Button>
+        }
+        
+      </div>
     </div>
   )
 }
