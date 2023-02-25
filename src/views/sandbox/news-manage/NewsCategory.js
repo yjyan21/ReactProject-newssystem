53e53f822ed12ef7b7ef4f7348a78f0f88275b33
yjyan21 <yjyan1987@gmail.com>
2023-02-25 14:33:22 -0800

    update NewsCategory

--- src/views/sandbox/news-manage/NewsCategory.js
+++ src/views/sandbox/news-manage/NewsCategory.js
@@ -1,7 +1,184 @@
-import React from 'react'
+import React, {useState, useEffect, useRef, useContext} from 'react'
+import {Table, Button, Modal, Form, Input} from 'antd'
+import {DeleteOutlined, ExclamationCircleFilled} from '@ant-design/icons'
+
+import axios from 'axios'
+
+const { confirm } = Modal;
 
 export default function NewsCategory() {
+  const [dataSource, setDataSource] = useState([])
+  useEffect(()=>{
+    axios.get("/categories").then
+    (res=> {
+      setDataSource(res.data)
+    })
+},[])
+  const handleSave = (record) => {
+    //console.log(record)
+    setDataSource(dataSource.map(item=>{
+      if(item.id===record.id){
+        return {
+          id:item.id,
+          title:record.title,
+          value:record.value
+        }
+      }
+      return item
+    }))
+    axios.patch(`/categories/${record.id}`,{
+      title:record.title,
+      value:record.value
+    })
+  }
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      render:(id)=>{
+        return <b>{id}</b>
+      }
+    },
+    {
+      title: 'News Category',
+      dataIndex: 'title',
+      onCell: (record) => ({
+        record,
+        editable: true,
+        dataIndex: 'title',
+        title: 'News Category',
+        handleSave:handleSave
+      }),
+    },
+    {
+      title: '操作',
+      render:(item)=>{
+        return (
+          <div>
+            <Button danger shape="circle" icon={<DeleteOutlined />} 
+            onClick={()=>{ //这里的形参括号里面不能写item,否则传下去的参数就不一样了
+              confirmMethod(item);
+            }}/>
+          </div>)
+        }
+    },
+  ];
+
+  const confirmMethod = (item) => {
+    confirm({
+      title: 'Do you Want to delete these items?',
+      icon: <ExclamationCircleFilled />,
+      // content: 'Some descriptions',
+      onOk() {
+        // console.log('OK');
+        deleteMethod(item);
+      },
+      onCancel() {
+        // console.log('Cancel');
+      },
+    });
+  };
+  const deleteMethod = (item) => {
+    //console.log(item)
+      setDataSource(dataSource.filter(data => data.id !== item.id))
+      axios.delete(`categories/${item.id}`)
+  }
+
+  const EditableContext = React.createContext(null);
+
+  const EditableRow = ({ index, ...props }) => {
+    const [form] = Form.useForm();
+    return (
+      <Form form={form} component={false}>
+        <EditableContext.Provider value={form}>
+          <tr {...props} />
+        </EditableContext.Provider>
+      </Form>
+    );
+  };
+  const EditableCell = ({
+    title,
+    editable,
+    children,
+    dataIndex,
+    record,
+    handleSave,
+    ...restProps
+  }) => {
+    const [editing, setEditing] = useState(false);
+    const inputRef = useRef(null);
+    const form = useContext(EditableContext);
+    useEffect(() => {
+      if (editing) {
+        inputRef.current.focus();
+      }
+    }, [editing]);
+    const toggleEdit = () => {
+      setEditing(!editing);
+      form.setFieldsValue({
+        [dataIndex]: record[dataIndex],
+      });
+    };
+    const save = async () => {
+      try {
+        const values = await form.validateFields();
+        toggleEdit();
+        handleSave({
+          ...record,
+          ...values,
+        });
+      } catch (errInfo) {
+        console.log('Save failed:', errInfo);
+      }
+    };
+    let childNode = children;
+    if (editable) {
+      childNode = editing ? (
+        <Form.Item
+          style={{
+            margin: 0,
+          }}
+          name={dataIndex}
+          rules={[
+            {
+              required: true,
+              message: `${title} is required.`,
+            },
+          ]}
+        >
+          <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+        </Form.Item>
+      ) : (
+        <div
+          className="editable-cell-value-wrap"
+          style={{
+            paddingRight: 24,
+          }}
+          onClick={toggleEdit}
+        >
+          {children}
+        </div>
+      );
+    }
+    return <td {...restProps}>{childNode}</td>;
+  };
+
   return (
-    <div>NewsCategory</div>
+    <div>
+      <Table 
+        components={{
+          body: {
+            row: EditableRow,
+            cell: EditableCell,
+          },
+        }}
+
+       dataSource={dataSource} columns={columns} 
+       pagination={{
+        pageSize:5
+       }}
+       rowKey={item=>item.id}
+       />
+    </div>
   )
 }
